994acdd37efe71c09270c3c59341c33823018301
Michael Evans <evansrichardmichael@gmail.com>
2022-07-13 18:36:56 -0600

    add assets

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 
+let capitalizeFirstLetter = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+let typeToIcon = (type) => {
+  if (type === 'fairy') {
+    return 'AAAAAAA'
+  }
+  return type
+}
+
 export default function PokemonCards({ pokemon }) {
   return (
-    <div className="d-flex m-10">
-      <div className="flex flex-col p-10 border-gray-700 border-2 ">
-        <div className="flex flex-row">
+    <div className="d-flex m-10 max-w-sm">
+      <div className="flex flex-col p-10 border-gray-700 border-2">
+        <div className="flex flex-row w-full">
           {pokemon.types.map((p) => (
-            <h3 className="pr-2">{p.type.name}</h3>
+            <div className="flex justify-between">
+              <h3 className="pr-2">{typeToIcon(p.type.name)}</h3>
+            </div>
           ))}
+          <h3 className="text-right w-full"># {pokemon.id}</h3>
         </div>
-        <div className="w-100 h-52  overflow-hidden">
+        <div className="w-full h-52 overflow-hidden">
           <img
             src={pokemon.sprites.front_default}
             alt={pokemon.name}
-            className="w-72 h-72 object-cover pb-20 "
+            className="inline w-56 h-56 object-cover"
           ></img>
         </div>
-        <h2 className="border-2 text-left">{pokemon.name}</h2>
+        <h1 className="text-left font-medium text-lg">
+          {capitalizeFirstLetter(pokemon.name)}
+        </h1>
       </div>
     </div>
   )
